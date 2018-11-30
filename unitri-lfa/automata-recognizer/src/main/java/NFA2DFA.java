ea8119476fde8b775177c26bd26bbf6e72872f39
Lucas Gontijo <lucasgontijo@whoami.zuppers.intra.net>
2018-11-30 15:37:45 -0200

    Added mapping for new transactions, added mapping to dfa table, added dfa validation in test.

--- unitri-lfa/automata-recognizer/src/main/java/NFA2DFA.java
+++ unitri-lfa/automata-recognizer/src/main/java/NFA2DFA.java
@@ -7,6 +7,8 @@ public class NFA2DFA {
     private List<String> finalStates;
     private NFA nfa;
 
+    private Stack<String> unmappedStates = new Stack<>();
+
     public NFA2DFA(List<String> alphabet, String start, List<String> finalStates, NFA nfa) {
         this.alphabet = alphabet;
         this.start = start;
@@ -22,6 +24,7 @@ public class NFA2DFA {
 
         while (hasUnmmapedStates(table)) {
             mapNewStates(table);
+            mapNewTransitions(table);
         }
 
         return mapToDfaTable(table);
@@ -29,11 +32,14 @@ public class NFA2DFA {
 
     private Boolean hasUnmmapedStates(Map<String, Map<String, String>> dfa) {
 
+        this.unmappedStates.clear();
+
         final Set<String> keys = dfa.keySet();
 
         for (Map<String, String> value : dfa.values()) {
             for (String transition : value.values()) {
                 if (!keys.contains(transition)) {
+                    this.unmappedStates.add(transition);
                     return true;
                 }
             }
@@ -44,37 +50,74 @@ public class NFA2DFA {
 
     private void mapNewStates(Map<String, Map<String, String>> dfa) {
 
+        Map<String, String> symbolTransitions = new HashMap<>();
+        alphabet.forEach(x -> symbolTransitions.put(x, ""));
+
+        while (!unmappedStates.isEmpty()) {
+            String newKey = unmappedStates.pop();
+            dfa.put(newKey, symbolTransitions);
+        }
+    }
+
+    private void mapNewTransitions(Map<String, Map<String, String>> table) {
+
+        table.forEach((state, transitions) -> {
+
+                    Map<String, String> unmappedTransitions = new HashMap<>();
+
+                    transitions.forEach((symbol, transition) ->
+                            mapEmptyTransitions(state, unmappedTransitions, symbol, transition));
+
+                    putNewTransitions(table, state, unmappedTransitions);
+                }
+        );
+    }
+
+    private void putNewTransitions(Map<String, Map<String, String>> table, String state, Map<String, String> unmappedTransitions) {
+        unmappedTransitions.forEach((sym, tra) -> table.put(state, unmappedTransitions));
+    }
+
+    private void mapEmptyTransitions(String state, Map<String, String> unmappedTransitions, String symbol, String transition) {
+        if (transition.isEmpty()) {
+
+            StringBuilder newTransition = new StringBuilder();
+
+            if (state.length() > 2) {
+                List<String> states = symbolSplit(state);
+                states.forEach(s -> newTransition.append(getTransition(s, symbol)));
+            }
+
+            unmappedTransitions.put(symbol, newTransition.toString());
+        }
     }
 
     private Map<String, String> mapToDfaTable(Map<String, Map<String, String>> table) {
 
+        final Set<String> keys = table.keySet();
+
         Map<String, String> dfa = new HashMap<>();
 
-        //TODO: lógica de:para
+        table.forEach((state, transitions) ->
+                transitions.forEach((symbol, transition) -> {
+                    dfa.put(String.format("%s->%s", state, symbol), transition);
+                })
+        );
 
-        // VAI DAR PROBLEMA DE CONCORRENCIA AO TENTAR MODIFICAR E ACESSAR
-        // PRIMEIRO MAPEA OS NOVOS ESTADOS
-        // SEGUNDO PEGA AS TRANSIÇÕES PRA CADA UM DOS SIMBOLOS DO ALFABETO
-        // ADICIONA NO MAPA
-        // MAPEAR OS NOVOS ESTADOS...
-
-        dfa.put("q0q1q2", "q0");
-
-        mapStartEndFinalStates(dfa);
+        mapStartEndFinalStates(keys, dfa);
 
         return dfa;
     }
 
-    private void mapStartEndFinalStates(Map<String, String> dfa) {
+    private void mapStartEndFinalStates(Set<String> keys, Map<String, String> dfa) {
 
-        List<String> endStates = new ArrayList<>();
+        StringBuilder endStates = new StringBuilder();
 
         dfa.put("initial", start);
 
-        for (String key : dfa.keySet()) {
+        for (String key : keys) {
             for (String state : this.finalStates) {
                 if (key.contains(state)) {
-                    endStates.add(key);
+                    endStates.append(key);
                 }
             }
         }
@@ -101,5 +144,9 @@ public class NFA2DFA {
                 .replace(",", "")
                 .replace("]", "")
                 .replace(" ", "");
+    }
+
+    private List<String> symbolSplit(String symbol) {
+        return Arrays.asList(symbol.split("(?<=\\G.{2})"));
     }
 }
--- unitri-lfa/automata-recognizer/src/test/NFA2DFATest.java
+++ unitri-lfa/automata-recognizer/src/test/NFA2DFATest.java
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class NFA2DFATest {
 
     private ArrayListMultimap<String, String> table;
+    private Map<String, String> expectedOut;
 
     @BeforeEach
     void setUp() {
+
         table = ArrayListMultimap.create();
         table.put("initial", "q0");
         table.put("final", "q2");
@@ -20,6 +23,16 @@ class NFA2DFATest {
         table.put("q0->b", "q0");
         table.put("q0->a", "q1");
         table.put("q1->a", "q2");
+
+        expectedOut = new HashMap<>();
+        expectedOut.put("initial", "q0");
+        expectedOut.put("final", "q0q1q2");
+        expectedOut.put("q0q1->a", "q0q1q2");
+        expectedOut.put("q0q1->b", "q0");
+        expectedOut.put("q0q1q2->a", "q0q1q2");
+        expectedOut.put("q0q1q2->b", "q0");
+        expectedOut.put("q0->b", "q0");
+        expectedOut.put("q0->a", "q0q1");
     }
 
     @Test
@@ -35,11 +48,12 @@ class NFA2DFATest {
         NFA nfa = new NFA(this.table);
 
         NFA2DFA nfa2DFA = new NFA2DFA(alphabet, "q0", finalStates, nfa);
+        Map<String, String> out = nfa2DFA.convert();
 
-        Map<String, String> dfa = nfa2DFA.convert();
+        DFA dfa = new DFA(out);
 
-        Assert.assertNotNull(dfa);
-        Assert.assertFalse(dfa.isEmpty());
-
+        Assert.assertNotNull(out);
+        Assert.assertEquals(expectedOut, out);
+        Assert.assertTrue(dfa.extension("baa"));
     }
 }
